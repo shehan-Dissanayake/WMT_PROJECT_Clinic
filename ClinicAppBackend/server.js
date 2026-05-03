e98da8d4c53894e7138e10e93d546bf67981d0a5
IT24104090 <shehanr900@gmail.com>
2026-05-03 11:54:49 +0530

    update port binding

--- ClinicAppBackend/server.js
+++ ClinicAppBackend/server.js
@@ -40,7 +40,7 @@ app.get('/', (req, res) => {
     res.send('Welcome to the Clinic App Backend API!');
 });
 
-// Start the Server
-app.listen(PORT, () => {
-    console.log(`🚀 Server is running on http://localhost:${PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
 });
